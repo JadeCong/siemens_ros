2c8410ccd67eb87b5695db9f125462db862a54e0
Jade Cong <jade.cong@qq.com>
2024-09-07 16:52:17 +0800

    update node message quene size

--- siemens_plc/scripts/siemens_plc_interface.py
+++ siemens_plc/scripts/siemens_plc_interface.py
@@ -60,7 +60,7 @@ def siemens_plc_interface_node():
     
     # define the publisher for writing laser config parameters to modbus registers.
     pub_laser_config = rospy.Publisher(sub_topic, HoldingRegister,
-                                        queue_size=10,
+                                        queue_size=1,
                                         tcp_nodelay=True,
                                         latch=False)
     laser_config = HoldingRegister()
@@ -69,7 +69,7 @@ def siemens_plc_interface_node():
     sub_laser_switch = rospy.Subscriber(laser_switch_topic, Bool,
                                         callback=check_laser_switch_callback, 
                                         callback_args=[pub_laser_config, laser_config], 
-                                        queue_size=10, 
+                                        queue_size=1, 
                                         tcp_nodelay=True)
     
     # spin() simply keeps python from exiting until this node is stopped
