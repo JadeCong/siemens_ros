3c55f8398ebc8aaf803753ee331d769d2ff7f9a0
Jade Cong <jade.cong@qq.com>
2024-09-10 00:18:05 +0800

    update setup.py

--- siemens_plc/setup.py
+++ siemens_plc/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['siemens_plc'],
     package_dir={'': 'src'},
-    requires=['rospy', 'dynamic_reconfigure', 'std_msgs', 'modbus']
+    # requires=['rospy', 'dynamic_reconfigure', 'std_msgs', 'modbus']
 )
 
 setup(**d)
